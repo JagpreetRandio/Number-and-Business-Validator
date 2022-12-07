22b541699f3382e550c803ab953621c8c12bd8e5
Jason Chen <jason.ccfu@gmail.com>
2022-12-06 20:50:16 -0800

    added the number vaildator

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,3 @@
-var numberEl = document.getElementById();
-var searchEl = document.getElementById();
 var APIKey = "42d8743ba62a4bad80b362d999437d44";
 
 function getNumber(number) {
@@ -24,3 +22,29 @@ const options = {
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+
+
+//Phone number validation
+
+//when click 'submit'
+document.getElementById('myform').addEventListener('submit', validateForm);
+
+//validate the number formate
+function validatePhoneNumber(input_str) {
+  var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+  //return the formate
+  return re.test(input_str);
+}
+//show the error or success
+function validateForm(event) {
+  var phone = document.getElementById('myform_phone').value;
+  if (!validatePhoneNumber(phone)) {
+      document.getElementById('phone_error').classList.remove('hidden');
+  } else {
+      document.getElementById('phone_error').classList.add('hidden');
+      alert("validation success")
+  }
+  event.preventDefault();
+}
