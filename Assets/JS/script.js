70ab1538e8cc70d8b1a697a629ba991292603fb0
Jason Chen <jason.ccfu@gmail.com>
2022-12-06 19:16:21 -0800

    added another api

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,20 @@
+var numberEl = document.getElementById();
+var searchEl = document.getElementById();
+var APIKey = "42d8743ba62a4bad80b362d999437d44";
+
+function getNumber(number) {
+    
+    var URL =
+      "https://scrape.abstractapi.com/v1/?api_key=" +
+      APIKey +
+      "&url=https://news.ycombinator.com";
+  
+    fetch(baseURL).then(function (res) {
+      return res.json().then(function (data) {});
+    });
+  }
+
+
 const options = {
     method: 'GET',
     headers: {accept: 'application/json', Authorization: '2064126593'}
