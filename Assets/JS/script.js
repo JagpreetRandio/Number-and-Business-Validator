641a7f959016910eb2bf7790cdff35100cad3402
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-12-08 01:27:04 -0800

    finished with styling webpage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -13,15 +13,15 @@ function getNumber(number) {
   }
 
 
-const options = {
-    method: 'GET',
-    headers: {accept: 'application/json', Authorization: '2064126593'}
-  };
+// const options = {
+//     method: 'GET',
+//     headers: {accept: 'application/json', Authorization: '2064126593'}
+//   };
   
-  fetch('https://api.yelp.com/v3/businesses/search/phone', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+//   fetch('https://api.yelp.com/v3/businesses/search/phone', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
 
 
 
