711b8702315b33744823e237413318c665d50390
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-12-08 15:44:53 -0800

    struggling on the JS code, please check over it

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,17 +1,16 @@
-var APIKey = "42d8743ba62a4bad80b362d999437d44";
+// var APIKey = "42d8743ba62a4bad80b362d999437d44";
 
-function getNumber(number) {
+// function getNumber(number) {
     
-    var URL =
-      "https://scrape.abstractapi.com/v1/?api_key=" +
-      APIKey +
-      "&url=https://news.ycombinator.com";
+//     var URL =
+//       "https://scrape.abstractapi.com/v1/?api_key=" +
+//       APIKey +
+//       "&url=https://news.ycombinator.com";
   
-    fetch(baseURL).then(function (res) {
-      return res.json().then(function (data) {});
-    });
-  }
-
+//     fetch(baseURL).then(function (res) {
+//       return res.json().then(function (data) {});
+//     });
+//   } 
 
 // const options = {
 //     method: 'GET',
@@ -22,8 +21,6 @@ function getNumber(number) {
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
-
-
 
 //Phone number validation
 
@@ -49,4 +46,55 @@ function validateForm(event) {
   event.preventDefault();
 }
 
+// $.getJSON("https://phonevalidation.abstractapi.com/v1/?api_key=0879c0a3573c4b6399e5f11245ecee00&phone=14152007986", function(data) {
 
+//     console.log("display content")
+//     console.log(data);
+// })
+
+
+var getPhoneNumber = function(phoneNumber){
+  var apiKey= "0879c0a3573c4b6399e5f11245ecee00";
+  var apiURL = `https://phonevalidation.abstractapi.com/v1/?api_key=${apiKey}&phone=${phoneNumber}`
+
+  fetch(apiURL)
+  .then(function(response){
+      response.json().then(function(data){
+         displayPhoneNumber(data);
+      });
+  });
+};
+
+var numberInput =document.getElementById("myform_phone");
+var resultContainer =document.querySelector("result-container");
+
+var sumbitHandler = function(event){
+  event.preventDefault();
+  var phoneNumber = numberInput.value.trim();
+  if(phoneNumber){
+    // phone data 
+      getData(phoneNumber);
+  } else{
+      //an alert will show up if you have not entered in a phone number
+      alert("Please enter a phone number");
+  }
+  saveSearch();
+}
+
+
+var numberInputEl=document.getElementById("myform_phone");
+function getNumberData(){
+  results.innerHTML=""
+  
+  var phoneNumber = numberInputEl.value;
+  var apiKey= "0879c0a3573c4b6399e5f11245ecee00";
+  var apiURL = `https://phonevalidation.abstractapi.com/v1/?api_key=${apiKey}&phone=${phoneNumber}`;
+  console.log(apiURL + apiKey)
+  fetch(apiURL + apiKey)
+  .then(function(res){
+      return res.json()
+  })
+  .then(function(data){
+      createResults(data);
+  })
+}
