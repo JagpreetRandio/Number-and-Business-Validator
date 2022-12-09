168b16b045d9899d318ee161d95ed9461da476a9
Jason <mobilepc35@gmail.com>
2022-12-08 19:04:52 -0800

    updates area code lookup

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,52 +1,61 @@
-var APIKey = "42d8743ba62a4bad80b362d999437d44";
+var supportedAreaCodes = [
+    {
+        areacode:  "206",
+        timezone: "GMT+8",
+        location: "Seattle, WA"
+    },
+    {
+        areacode:  "919",
+        timezone: "GMT+5",
+        location: "Raleigh, NC"
+    },
+    {
+        areacode:  "607",
+        timezone: "GMT+5",
+        location: "Ithaca, NY"
+    }
+];
 
-function getNumber(number) {
-    
-    var URL =
-      "https://scrape.abstractapi.com/v1/?api_key=" +
-      APIKey +
-      "&url=https://news.ycombinator.com";
-  
-    fetch(baseURL).then(function (res) {
-      return res.json().then(function (data) {});
-    });
-  }
 
-
-// const options = {
-//     method: 'GET',
-//     headers: {accept: 'application/json', Authorization: '2064126593'}
-//   };
+const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', Authorization: '2067826593'}
+  };
   
 //   fetch('https://api.yelp.com/v3/businesses/search/phone', options)
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
+   var searchForm = document.getElementById ("myform");
+   var phoneInput = document.getElementById("myform_phone")
+   searchForm.addEventListener ('submit', handlePhoneSearch);
 
+   function handlePhoneSearch(event) {
+    console.log ( " form submitted, in function handlePhoneSearch")
+    if (!phoneInput.value ){
+        return;
+    }
+    event.preventDefault();
+    var phoneNum  = phoneInput.value.trim();
+    console.log ( "phoneNum = ", phoneNum);
 
-//Phone number validation
-
-//when click 'submit'
-document.getElementById('myform').addEventListener('submit', validateForm);
-
-//validate the number formate
-function validatePhoneNumber(input_str) {
-  var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-
-  //return the formate
-  return re.test(input_str);
-}
-//show the error or success
-function validateForm(event) {
-  var phone = document.getElementById('myform_phone').value;
-  if (!validatePhoneNumber(phone)) {
-      document.getElementById('phone_error').classList.remove('hidden');
-  } else {
-      document.getElementById('phone_error').classList.add('hidden');
-      alert("validation success")
-  }
-  event.preventDefault();
+    // call any timzone / area code function
+    analyseTimeZone(phoneNum);
+    // now call any validator function
+    phoneInput.value = '';
 }
 
+function analyseTimeZone(phone) {
+    console.log("in analyseTimeZone")
+    console.log("phone number is = ", phone);
+    var areaCode = phone.split('-')[0];
+    console.log("area code  is = ", areaCode);
 
+    for (var i=0; i < supportedAreaCodes.length; i++){
+        if (areaCode === supportedAreaCodes[i].areacode){
+            console.log("Found the location!! location : ", supportedAreaCodes[i].location)
+        }
+    }
+
+}
